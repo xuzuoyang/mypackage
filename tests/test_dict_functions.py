810e4952417a327e70fef6ed75beab9c6e525c0b
xuzuoyang <xuzuoyang@gmail.com>
2018-12-18 13:19:48 +0800

    add test

---
+++ tests/test_dict_functions.py
@@ -0,0 +1,12 @@
+from .context import mypackage
+from mypackage import dict_substract
+
+
+class TestDictUtils():
+    '''Test dict related functions.
+    '''
+    def test_substract(self):
+        dict1 = {'a': 11, 'b': 0}
+        dict2 = {'b': 0}
+
+        assert dict_substract(dict1, dict2) == {'a': 11}
