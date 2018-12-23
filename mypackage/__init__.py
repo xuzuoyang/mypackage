286e0b400d3026149f46156c6652b3eebbcbbfba
xuzuoyang <xuzuoyang@gmail.com>
2018-12-23 09:36:51 +0800

    delete outers for cookiecutter branch

--- mypackage/__init__.py
+++
@@ -1,6 +0,0 @@
-from .core import dict_substract
-
-
-__all__ = [
-    'dict_substract',
-]
--- tests/context.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.abspath('..'))
-
-import {{cookiecutter.project_slug}}
